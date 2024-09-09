7720710c4aee688acab77f706548b4c60bf5441d
Xin Huang <142068979+carriehxx@users.noreply.github.com>
2024-09-09 13:50:04 +0800

    Updates

---
+++ assets/AboutView-DyK2KTWr.js
@@ -0,0 +1,1 @@
+import{c as a,a as e,b as t,o,t as c,A as s}from"./index-BzDJTTOZ.js";const _={__name:"AboutView",setup(n){return(r,l)=>(o(),a("main",null,[e(c),t("section",null,[e(s)])]))}};export{_ as default};
