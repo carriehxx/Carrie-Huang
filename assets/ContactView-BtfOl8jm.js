db083d27eb1b04cbd0fbac69fad5d6b1424cffa9
Xin Huang <142068979+carriehxx@users.noreply.github.com>
2024-09-09 15:15:12 +0800

    Updates

---
+++ assets/ContactView-BtfOl8jm.js
@@ -0,0 +1,1 @@
+import{c as a,a as e,b as t,o as c,t as o,C as n}from"./index-C7SGI3G1.js";const m={__name:"ContactView",setup(s){return(r,l)=>(c(),a("main",null,[e(o),t("section",null,[e(n)])]))}};export{m as default};
